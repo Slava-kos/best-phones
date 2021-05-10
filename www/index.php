3922d32070c38b0c42ff1ebd2fd9e6b50d3f738c
sviataslau <slvkosilo@gmail.com>
2021-05-10 20:20:00 +0300

    add controllers

--- www/index.php
+++ www/index.php
@@ -1,3 +1,11 @@
 <?php
-    phpinfo();
 
+//определяется с каким контроллером будет проводиться работа
+$controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Index';
+
+echo 'Подключаемый php файл (Контроллер) = ' . $controllerName . '<br />';
+
+//определение функции с которой будет производиться работа
+$actionName = isset ($_GET['action']) ? $_GET['action'] : 'index';
+
+echo 'функция формирующая страницу (Экшн) = ' . $actionName . '<br />';
