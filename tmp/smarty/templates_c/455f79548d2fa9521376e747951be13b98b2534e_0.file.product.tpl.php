9a212120e3be7fe096fa023b19f7d93f2048f647
sviataslau <slvkosilo@gmail.com>
2021-05-19 13:20:18 +0300

    Добавлено удаление из корзины

--- tmp/smarty/templates_c/455f79548d2fa9521376e747951be13b98b2534e_0.file.product.tpl.php
+++ tmp/smarty/templates_c/455f79548d2fa9521376e747951be13b98b2534e_0.file.product.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2021-05-18 22:30:41
+/* Smarty version 3.1.36, created on 2021-05-19 12:18:47
   from 'C:\xampp\htdocs\best-phones.local\views\default\product.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_60a423f13c0e04_17884189',
+  'unifunc' => 'content_60a4e607992f79_27784643',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '455f79548d2fa9521376e747951be13b98b2534e' => 
     array (
       0 => 'C:\\xampp\\htdocs\\best-phones.local\\views\\default\\product.tpl',
-      1 => 1621369820,
+      1 => 1621419524,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_60a423f13c0e04_17884189 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60a4e607992f79_27784643 (Smarty_Internal_Template $_smarty_tpl) {
 ?><h3><?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['name'];?>
 </h3>
     
@@ -30,9 +30,12 @@ function content_60a423f13c0e04_17884189 (Smarty_Internal_Template $_smarty_tpl)
  Наличие: <?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['status'];?>
 
 
+<a id="removeCart_<?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['id'];?>
+" <?php if (!$_smarty_tpl->tpl_vars['itemInCart']->value) {?>class="hideme"<?php }?> href="#" onClick="removeFromCart(<?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['id'];?>
+); return false;" alt="Удалить из корзины">Удалить из корзины</a>
 <a id="addCart_<?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['id'];?>
-"  href="#" onClick="addToCart(<?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['id'];?>
-); return false;" alt="Добавить в корзину">Добавить в корзину</a>
+" <?php if ($_smarty_tpl->tpl_vars['itemInCart']->value) {?>class="hideme"<?php }?> href="#" onClick="addToCart(<?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['id'];?>
+); return false;" alt="Добавить в корзину">Добавить в корзину</a>  
 <p> Описание <br /><?php echo $_smarty_tpl->tpl_vars['rsProduct']->value['description'];?>
 </p>
 <?php }
